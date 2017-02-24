2db6642b8357ad5ce246d133a90042c3e66ebadd
Arunav Gupta <firehead2468@users.noreply.github.com>
2017-02-23 18:07:24 -0800

    add inVision UI

---
+++ CentriPEDAL/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CentriPEDAL
+//
+//  Created by Arunav Gupta on 12/3/16.
+//  Copyright © 2016 Arunav-Gupta. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textFieldWithPlaceholder: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension UITextField{
+    @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSForegroundColorAttributeName: newValue!])
+        }
+    }
+}
+
