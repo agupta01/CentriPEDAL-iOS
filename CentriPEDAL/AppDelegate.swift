66df382d1f99de9d71473347b405212c00dbb6ea
Arunav Gupta <firehead2468@users.noreply.github.com>
2017-02-24 22:02:51 -0800

    complete new acccount function

--- CentriPEDAL/AppDelegate.swift
+++ CentriPEDAL/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 import Firebase
+import FirebaseAuth
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- CentriPEDAL/SignUp.swift
+++ CentriPEDAL/SignUp.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class SignUp: UIViewController {
     
@@ -16,18 +17,32 @@ class SignUp: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var confirmPassField: UITextField!
     
-    @IBAction func signUpAction (_ sender: AnyObject) {
-        if passwordField == confirmPassField {
-            FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
-                // ...
-            }
-
-        } else {
-            // Somehow print that password and confirmPass don't match
-        }
+    @IBAction func createNewAccountAction(_ sender: AnyObject) {
         
+        if emailField.text == "" && passwordField.text != confirmPassField.text {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            FIRAuth.auth()?.createUser(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
-    
-    
-    
 }
